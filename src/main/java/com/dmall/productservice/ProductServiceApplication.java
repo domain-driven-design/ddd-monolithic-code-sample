b1294878eceec8b05b1748acbff745ec4a771979
Lin Ning <nlin@thoughtworks.com>
2019-10-14 22:09:25 +0800

    remove useless files

--- src/main/java/com/dmall/productservice/ProductServiceApplication.java
+++ src/main/java/com/dmall/productservice/ProductServiceApplication.java
@@ -30,6 +30,4 @@ public class ProductServiceApplication {
                 .build();
 
     }
-
-
 }
--- update-contract.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git submodule init
-git submodule update
-git submodule foreach git checkout master
-git submodule foreach git pull
